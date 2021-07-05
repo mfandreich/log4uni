bdb521f8856e7b3cf6df1ef1fc1ebd730def586f
mfandreich <mfandreich@users.noreply.github.com>
2021-07-05 13:33:12 +0700

    Update release.sh

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -2,6 +2,7 @@
 TAG_NAME=$(strings ./Build/Release/log4uni.dll | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | egrep -o '^[0-9]+\.[0-9]+\.[0-9]+' ) 
 echo "Target version tag name $TAG_NAME"
 git subtree split -P Build/Release -b upm
+git log --pretty=format:"%H" upm --
 TAG_BRANCH_COMMIT=$(git log --max-count=1 --pretty=format:"%H" upm --)
 echo "Git subtree commit $TAG_BRANCH_COMMIT"
 git push -f origin upm
