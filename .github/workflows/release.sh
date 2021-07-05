2d1a3d8aba3e8e8db1542311eb272cf36ca0cb82
mfandreich <mfandreich@users.noreply.github.com>
2021-07-05 19:03:25 +0700

    Update release.sh

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -3,10 +3,10 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 TAG_NAME=$(strings ./Build/Release/log4uni.dll | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | egrep -o '^[0-9]+\.[0-9]+\.[0-9]+' ) 
 echo "Target version tag name $TAG_NAME found. Create subtree..."
-git subtree split -P Build/Release -b upm >> /dev/null
+git subtree split -P Build/Release -b upm >> /dev/null 2>&1
 TAG_BRANCH_COMMIT=$(git log --max-count=1 --pretty=format:"%H" upm --)
 echo "Git subtree commit $TAG_BRANCH_COMMIT."
-git push -f origin upm >> /dev/null
+git push -f origin upm >> /dev/null 2>&1
 EXISTING_TAG=$(git tag -l $TAG_NAME)
 echo "Existing version tag name $EXISTING_TAG found."
 if [ "$EXISTING_TAG" = "$TAG_NAME" ];
